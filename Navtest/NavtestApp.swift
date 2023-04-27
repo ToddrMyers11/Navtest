605de27d74878b3abaa9cd4fd6f7282fc5415e1f
Todd <toddrmyers11@gmail.com>
2023-04-27 16:56:21 -0400

    Initial Commit

---
+++ Navtest/NavtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavtestApp.swift
+//  Navtest
+//
+//  Created by Todd Myers on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct NavtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
